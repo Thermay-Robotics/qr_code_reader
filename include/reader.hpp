9da12691107705efcfc94a3b879621dc5c3d16c6
totordudu <toto.duvinage@gmail.com>
2022-04-07 22:59:10 +0200

    create reader class to add publisher

---
+++ include/reader.hpp
@@ -0,0 +1,42 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <geometry_msgs/Point.h>
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <zbar.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+// Include message
+#include "qr_code_reader/qr_msg.h"
+
+class Reader
+{
+private:
+    ros::NodeHandle nh;
+    ros::Publisher qr_pub;
+    image_transport::Subscriber camera_sub;
+    image_transport::ImageTransport it;
+
+    void imageCallback(const sensor_msgs::ImageConstPtr& msg); 
+
+public:
+    Reader(/* args */);
+    ~Reader(){ cv::destroyAllWindows(); };
+
+    typedef struct
+    {
+        std::string type;
+        std::string data;
+        std::vector <cv::Point> location; 
+    }decodedObject;
+
+    void decode(cv::Mat &im, std::vector<decodedObject>&decodedObjects);
+};
+
+
--- src/reader.cpp
+++ src/reader.cpp
@@ -1,94 +1,87 @@
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
+#include "reader.hpp"
 
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <zbar.h>
 
-#include <opencv2/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-
-using namespace std;
-using namespace cv;
-using namespace zbar;
-
-typedef struct
+Reader::Reader() : it(nh)
 {
-  string type;
-  string data;
-  vector <Point> location;
-}decodedObject;
+    cv::namedWindow("view");
+    qr_pub = nh.advertise<qr_code_reader::qr_msg>("/QrCodeMsg", 1000);
+    camera_sub = it.subscribe("/usb_cam/image_raw", 1, &Reader::imageCallback, this);
+}
 
-// Find and decode barcodes and QR codes
-void decode(Mat &im, vector<decodedObject>&decodedObjects)
+void Reader::decode(cv::Mat &im, std::vector<decodedObject>&decodedObjects)
 {
 
   // Create zbar scanner
-  ImageScanner scanner;
+  zbar::ImageScanner scanner;
 
   // Configure scanner
-  scanner.set_config(ZBAR_QRCODE, ZBAR_CFG_ENABLE, 1);
+ scanner.set_config(zbar::ZBAR_QRCODE, zbar::ZBAR_CFG_ENABLE, 1);
 
   // Convert image to grayscale
-  Mat imGray;
-  cvtColor(im, imGray,CV_BGR2GRAY);
+  cv::Mat imGray;
+  cv::cvtColor(im, imGray,CV_BGR2GRAY);
 
   // Wrap image data in a zbar image
-  Image image(im.cols, im.rows, "Y800", (uchar *)imGray.data, im.cols * im.rows);
+  zbar::Image image(im.cols, im.rows, "Y800", (uchar *)imGray.data, im.cols * im.rows);
 
   // Scan the image for barcodes and QRCodes
   int n = scanner.scan(image);
 
   // Print results
-  for(Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
+  for(zbar::Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
   {
     decodedObject obj;
+    qr_code_reader::qr_msg msg;
 
     obj.type = symbol->get_type_name();
     obj.data = symbol->get_data();
 
+    msg.type = symbol->get_type_name();
+    msg.data = symbol->get_data();
+    
+    for(int i = 0;i < symbol->get_location_size();i++){
+      geometry_msgs::Point p;
+
+      p.x = symbol->get_location_x(i);
+      p.y = symbol->get_location_y(i);
+
+      msg.locations.push_back(p);
+
+      cv::Scalar black(0,0,0);
+      cv::Point center((int)p.x, (int)p.y);
+      cv::circle(im,center,10,black);
+
+      obj.location.push_back(center);
+    }
+
     // Print type and data
-    cout << "Type : " << obj.type << endl;
-    cout << "Data : " << obj.data << endl << endl;
+    std::cout << "Type : " << obj.type << std::endl;
+    std::cout << "Data : " << obj.data << std::endl;
+    std::cout << "Location : " << obj.location << std::endl;
     decodedObjects.push_back(obj);
+
+    qr_pub.publish(msg);
+
   }
 }
 
-
-
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+void Reader::imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
   try
   {
     cv::Mat img_mat = cv_bridge::toCvShare(msg, "bgr8")->image;
 
-    vector<decodedObject> decodedObjects;
+    std::vector<decodedObject> decodedObjects;
 
     decode(img_mat, decodedObjects);
 
-    imshow("view", img_mat);
+    cv::imshow("view", img_mat);
 
-    waitKey(30);
+    cv::waitKey(30);
     
   }
   catch (cv_bridge::Exception& e)
   {
     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
   }
-}
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "image_listener");
-  ros::NodeHandle nh;
-  cv::namedWindow("view");
-
-  image_transport::ImageTransport it(nh);
-  // Change topic depending on your camera
-  image_transport::Subscriber sub = it.subscribe("/usb_cam/image_raw", 1, imageCallback);
-  ros::spin();
-  cv::destroyAllWindows();
 }
